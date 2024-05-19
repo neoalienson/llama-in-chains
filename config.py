55bb808b434c19491cac1c1e57bf3e5f86a9ae61
Neo Alienson <neo@01man.com>
2024-05-19 16:40:40 +0800

    write default config

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
 from pyaml_env import parse_config, BaseConfig
+import os
 
-config = BaseConfig(parse_config('default.yaml'))
+config = parse_config('default.yaml')
+
+if os.path.isfile('config.yaml'):
+    config = {**config, **parse_config('config.yaml')}
+
+config = BaseConfig(config)
